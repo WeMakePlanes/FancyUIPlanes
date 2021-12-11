b9eff382a73489bdff64d397b0799dfe4b6a08bc
Nathan Peek <nmp14fsu@gmail.com>
2021-12-11 02:41:11 -0500

    Create router and add landing page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, RouteProps } from "react-router-dom";
 import './App.css';
+import Landing from './pages/Landing';
+
+const RouterPage = (
+  props: { pageComponent: JSX.Element } & RouteProps
+) => props.pageComponent;
 
 function App() {
   return (
-    <div className="App">
+    <Router>
 
-    </div>
+      <RouterPage path="/" pageComponent={<Landing />} />
+
+    </Router>
   );
 }
 
